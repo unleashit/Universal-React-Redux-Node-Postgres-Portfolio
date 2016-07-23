54084cb04b40fc3a73133881fb8c736ebbd24567
Jason Gallagher <unleashit@yahoo.com>
2016-07-23 15:51:28 -0700

    fixed routing so index doesn't take over

--- app/js/routes.js
+++ app/js/routes.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router';
 import App from './containers/App';
 import Home from './containers/Home';
 import NoMatch from './containers/NoMatch';
+import Dino from './containers/Dino';
 
 export default (
     <Route component={App}>
         <Route path='/' component={Home}>
-            <Route path=":hashRoute" component={Home} />
+            <Route path="/index:hashRoute" component={Home} />
         </Route>
+        <Route path="dino" component={Dino} />
         <Route path="*" component={NoMatch} />
     </Route>
 );
