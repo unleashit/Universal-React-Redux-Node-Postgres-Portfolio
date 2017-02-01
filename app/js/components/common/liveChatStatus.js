567913fe886e8580680cb6c1e5d2c252c2c04a94
JG <unleashit@yahoo.com>
2017-01-31 22:48:33 -0800

    small css tweak

--- app/js/components/common/liveChatStatus.js
+++ app/js/components/common/liveChatStatus.js
@@ -3,7 +3,7 @@ import React from 'react';
 const liveChatStatus = (props) => {
     return (
         <span className={props.remoteId ? 'chat-online pull-right' : 'chat-offline pull-right'}>
-            <i className="fa fa-cloud"></i> &nbsp;{props.remoteId ? 'I\'m online' : 'I\'m offline'}
+            <i className="fa fa-cloud" style={{paddingRight: "6px"}}></i>{props.remoteId ? 'I\'m online' : 'I\'m offline'}
         </span>
     );
 };
