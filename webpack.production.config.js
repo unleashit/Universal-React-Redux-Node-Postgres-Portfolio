d5026b394e91ccba0b0dbd0bd1440d54eb9ddd25
unleashit <unleashit@yahoo.com>
2018-02-26 18:12:42 -0800

    disable purify...

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,7 +5,7 @@ var webpack = require('webpack');
 var del = require('del');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-var Purify = require("purifycss-webpack-plugin");
+// var Purify = require("purifycss-webpack-plugin");
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 class CleanPlugin {
@@ -58,21 +58,21 @@ module.exports = {
             }
         }),
         new ExtractTextPlugin('./css/[name].min.css'),
-        new Purify({
-            basePath: __dirname,
-            paths: [
-                '/app/js/*.*'
-            ],
-            resolveExtensions: ['.js'],
-            purifyOptions: {
-                minify: false,
-                rejected: true,
-                whitelist: [
-                    '*nav*', '*navbar*', 'bg-inverse', '*live-chat*',
-                    '*input-group*', 'post-input', 'post-message', 'fa-pencil'
-                ]
-            }
-        }),
+        // new Purify({
+        //     basePath: __dirname,
+        //     paths: [
+        //         '/app/js/*.*'
+        //     ],
+        //     resolveExtensions: ['.js'],
+        //     purifyOptions: {
+        //         minify: false,
+        //         rejected: true,
+        //         whitelist: [
+        //             '*nav*', '*navbar*', 'bg-inverse', '*live-chat*',
+        //             '*input-group*', 'post-input', 'post-message', 'fa-pencil'
+        //         ]
+        //     }
+        // }),
         new OptimizeCssAssetsPlugin({
             cssProcessor: require('cssnano'),
             cssProcessorOptions: {discardComments: {removeAll: true, autoprefixer: false}},
