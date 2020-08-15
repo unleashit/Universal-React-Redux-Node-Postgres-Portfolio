ecda24538838d184726ae68a8ef715a890970909
unleashit <unleashit@protonmail.com>
2020-08-14 22:41:18 -0700

    update most deps

--- server/models/index.js
+++ server/models/index.js
@@ -30,7 +30,7 @@ fs.readdirSync(__dirname)
         );
     })
     .forEach(function(file) {
-        var model = sequelize['import'](path.join(__dirname, file));
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize)
         db[model.name] = model;
     });
 
