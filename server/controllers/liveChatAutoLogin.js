909f7b5400cdf48890b37709c094751666148743
unleashit <unleashit@protonmail.com>
2020-08-19 17:14:30 -0700

    add logout period after manual logout to helpdesk autologin

--- server/controllers/liveChatAutoLogin.js
+++ server/controllers/liveChatAutoLogin.js
@@ -8,65 +8,99 @@ const { liveChat: {
 
 let socket;
 let adminConnected = false;
+let tries = 5;
+const AUTOLOGIN_INTERVAL = 1000 * 60 * 4;
+const LOGOUT_TIME = 1000 * 60 * 15;
+
+// generate a basic random number to verify we're the admin
 const token = crypto.randomBytes(24).toString('base64');
 
 if (autoLogin.toLowerCase() === 'true') {
     socket = clientIo.connect(config.__SOCKET_IO_URL__);
 
     socket.on('connect', cronJob);
-    socket.on('disconnect', () => {
+    socket.on('chatDisconnected', () => {
         adminConnected = false;
+        tries = 0;
+
+        // after admin manually logs out,
+        // set period before autologin connects again
+        setTimeout(() => {
+            tries = 5;
+        }, LOGOUT_TIME)
     });
 }
 
 function cronJob() {
     console.log('ADMIN AUTOLOGIN CRON');
+
     const open = isOpen(autoLoginStart, autoLoginEnd, autoLoginExclude);
 
-    if (!adminConnected && open) {
-        adminLogin();
+    if (!adminConnected && open && tries > 0) {
+        tryToConnect(() => {
+            return new Promise((resolve) => {
+                socket.emit('admin login', { token }, id => {
+                    if (!id || id === 'unauthorized') {
+                        adminConnected = false;
+                        resolve(false);
+                    } else {
+                        console.log(`socket.io connected. Id: ${id}`);
+                        adminConnected = true;
+                        resolve(true);
+                    }
+                })
+            });
+        }, tries)
     }
 
     if (adminConnected && !open) {
         socket.disconnect();
     }
-    setTimeout(cronJob, 1000 * 60 * 4);
-};
+    setTimeout(cronJob, AUTOLOGIN_INTERVAL);
+}
 
+function tryToConnect(cb, tries = 5, waitFor = 3000) {
+    let connected = false;
+    let timer;
+    const originalTries = tries;
 
-function adminLogin() {
-    let tries = 10;
+    const connect =  async () => {
+        clearTimeout(timer);
 
-    const conn = () => {
-        if (adminConnected === false && tries > 0) {
-            setTimeout(() => {
-                socket.emit('admin login', { token }, id => {
-                    if (!id || id === 'unauthorized') {
-                        tries--;
-                        console.log(`ADMIN AUTO LOGIN attempts left: ${tries}`);
-                        conn();
-                    } else {
-                        adminConnected = true;
-                        console.log(`socket.io connected. Id: ${id}`);
-                    }
-                })
-            }, 3000);
+        if (connected === false && tries > 0) {
+            const result = await cb();
+
+            if (result) {
+                connected = true;
+                tries = originalTries;
+            } else {
+                tries--;
+                console.log(`tryToConnect attempts left: ${tries}`);
+                timer = setTimeout(connect, waitFor);
+            }
+
         } else {
-            console.log('auto admin login could not connect.');
+            console.log('tryToConnect could not connect.');
         }
     }
 
-    conn();
+    connect();
 }
 
-function isOpen(startTime = '9:00:00', endTime = '17:00:00', exclude = [6, 7]) {
+function isOpen(
+    startTime = '9:00:00',
+    endTime = '17:00:00',
+    exclude = [6, 7],
+    timeZone = 'America/Los_Angeles',
+) {
     if (typeof exclude === 'string') {
         exclude = exclude.trim().split(',');
     }
-    const [startH, startM, startS] = startTime.split(':');
-    const [endH, endM, endS] = endTime.split(':');
 
-    const PST = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
+    const [startH, startM, startS] = startTime.split(':').map(Number);
+    const [endH, endM, endS] = endTime.split(':').map(Number);
+
+    const PST = new Date().toLocaleString("en-US", { timeZone });
     const currentDate = new Date(PST);
 
     if (exclude.includes(currentDate.getDay())) {
@@ -89,4 +123,5 @@ function isOpen(startTime = '9:00:00', endTime = '17:00:00', exclude = [6, 7]) {
 exports.getAutoLoginToken = function() {
     return token;
 };
+
 
