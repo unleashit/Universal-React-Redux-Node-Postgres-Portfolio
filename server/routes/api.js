a8c4e4a9822539fd0b105c1c9d87c6f5723dfc07
unleashit <unleashit@protonmail.com>
2021-01-14 14:29:50 -0800

    remove cors

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var express = require('express');
-var _ = require('lodash');
 var bodyParser = require('body-parser');
 
 var portfolio = require('../controllers/portfolioAPI.js');
@@ -10,14 +9,15 @@ var contactForm = require('../controllers/contactForm.js');
 var router = express.Router();
 
 // CORS middleware for testing purposes
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,POST');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-};
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,POST');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// };
+//
+// router.use(allowCrossDomain);
 
-router.use(allowCrossDomain);
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
